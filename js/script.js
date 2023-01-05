787a786a858b33f79061d8d5f01e3687e630dcc1
JacekWarzecha <drjackys@gmail.com>
2023-01-05 11:52:33 +0100

    added basic JS & color placeholder

---
+++ js/script.js
@@ -0,0 +1,47 @@
+{
+  const tasks = [
+    {
+      content: "Przygotuj projekty",
+      done: false,
+    },
+    {
+      content: "Przygotuj cv",
+      done: true,
+    },
+  ];
+
+  const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+      htmlString += `
+        <li>
+        ${task.content}
+        </li>
+      `;
+    }
+
+    document.querySelector(".js-tasksList").innerHTML = htmlString;
+  };
+
+  const init = () => {
+    render();
+
+    const form = document.querySelector(".js-form");
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      const newTaskContent = document.querySelector(".js-newTask").value.trim();
+      if (newTaskContent === "") {
+        return;
+      }
+      tasks.push({
+        content: newTaskContent,
+      });
+
+      render();
+    });
+  };
+
+  init();
+}
